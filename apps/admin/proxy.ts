61c3e9db1a98b4d81f260a068088bc9f5b78eba2
Obi Anaedozie <dozie22@gmail.com>
2026-01-09 08:36:38 -0500

    chore: packages and ts-config updates

--- apps/admin/proxy.ts
+++ apps/admin/proxy.ts
@@ -1,4 +1,17 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { NextRequest } from "next/server";
 
-export default NextAuth(authConfig).auth(function proxy() {});
+type AppRouteHandlerFn = (
+  req: NextRequest,
+  ctx: { params: Promise<Record<string, string | string[]>> }
+) => void | Response | Promise<Response | void>;
+
+const { auth } = NextAuth(authConfig);
+
+export const proxy: AppRouteHandlerFn = auth(async () => {});
+
+console.log("Proxy running!");
+export const config = {
+  matcher: "/",
+};
